efbb933a9ce88b85e95bdf7ff9470833d833cc88
Erik Raae <erik@raae.eu>
2019-04-30 23:37:32 +0200

    Moved some logic to the StorageManager.

--- PromotionParser/Managers/StorageManager.cs
+++ PromotionParser/Managers/StorageManager.cs
@@ -6,18 +6,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PromotionParser.Managers
 {
-    class StorageManager
+    public class StorageManager
     {
-        private List<IPromotion> PromoList = new List<IPromotion>();
-        private readonly FileInfo filename;
-
-        public StorageManager(FileInfo filename)
-        {
-            this.filename = filename;
-        }
+        private List<IPromotion> _promoList = new List<IPromotion>();
 
         /// <summary>
         /// Replace the entire list of promotions
@@ -25,15 +20,28 @@ namespace PromotionParser.Managers
         /// <param name="promotions">The list of promotions to be saved.</param>
         public void SaveToMemory(List<IPromotion> promotions)
         {
-            PromoList = promotions;
+            if (_promoList.Count > 0 && _promoList != null)
+            {
+                if (MessageBox.Show("Vil du overskrive listen med aktiviteter som allerede ligger i minnet?", "Overskrive", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    _promoList = promotions;
+                }
+                else
+                {
+                    throw new Exception("Kunne ikke lagre aktiviteter til minnet");
+                }
+            }
         }
-
+        
         public void SaveToFile(List<IPromotion> result, FileInfo fileInfo)
         {
-            if (result.Count > 0)
+            if (MessageBox.Show(@"Vil du lagre resultatet som en xml-fil?", "Lagre", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
-                var f = new FileWriter(fileInfo);
-                f.WriteToFile();
+                if (result.Count > 0)
+                {
+                    var f = new FileWriter(fileInfo);
+                    f.WriteToFile();
+                }
             }
         }
 
@@ -41,9 +49,17 @@ namespace PromotionParser.Managers
         /// Load the entire list of promotions
         /// </summary>
         /// <returns>The entire list of promotions.</returns>
-        public List<IPromotion> Load()
+        public FileInfo ImportExcelFile()
         {
-            return null;
+            try
+            {
+                var fReader = new FileReader();
+                return fReader.ReadFromFile();
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
         }
     }
 }
