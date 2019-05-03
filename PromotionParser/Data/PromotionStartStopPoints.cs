9ff7d8004bb180332f8137a36413e8f9d3284ffb
Erik Raae <erik@raae.eu>
2019-05-03 17:26:55 +0200

    Renamed file

---
+++ PromotionParser/Data/PromotionStartStopPoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionParser.Data
+{
+    public class PromotionStartStopPoints
+    {
+        public int StartRow { get; set; }
+        public int EndRow { get; set; }
+    }
+}
