499ad6b04a4b34075872688d1260867a0a01fcaa
Erik Raae <erik@raae.eu>
2019-05-06 22:28:47 +0200

    + Nullcheck on the parsing result + Added the result as a property with nullcheck

--- PromotionParser/PromotionParser.cs
+++ PromotionParser/PromotionParser.cs
@@ -1,14 +1,29 @@
 ﻿using PromotionParser.Managers;
 using PromotionParser.Workers;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
+using PromotionParser.Data;
 using static System.String;
 
 namespace PromotionParser
 {
     public class PromotionParser
     {
+        private List<IPromotion> _promotions = new List<IPromotion>();
+        
+        public List<IPromotion> Promotions {
+            get
+            {
+                if (_promotions != null && _promotions.Count > 0)
+                {
+                    return _promotions;
+                }
+                throw new Exception("Listen er tom, kan ikke hente data.");
+            }
+        }
+        
         // Setup classes
         public void Run()
         {
@@ -17,11 +32,16 @@ namespace PromotionParser
                 var parser = new Parser(); // Initialize the xls file parser
                 var storage = new StorageManager(); // Initialize the storage manager
                 var importedExcelFile = StorageManager.ImportExcelFile(); // Load the file to memory
-            
+
                 parser.Parse(importedExcelFile); // Parse the xls file
+                
                 storage.SaveToMemory(parser.Result); // save the List<IPromotion> to the StorageManager.
+                if (parser.Result != null)
+                {
+                    _promotions = parser.Result;
+                }
                 StorageManager.SaveToFile(parser.Result, new FileInfo("ostekake.xml")); // Save the List to a xml file.
-            
+
                 // Generate report.
             }
             catch (Exception e)
